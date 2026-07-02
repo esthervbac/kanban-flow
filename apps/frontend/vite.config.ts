7336410133c351866f0fe96f8f5bfc621a53db73
Esther V Bacelar <esther.v.bacelar@gmail.com>
2026-07-01 23:09:42 -0300

    feat(): New adjusts to frontend work on deploy

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGithubPages = process.env.IS_GH_PAGES === "true";
+
 export default defineConfig({
-  base: "/",
+  base: isGithubPages ? "/kanban-flow/" : "/",
   plugins: [react()],
   server: {
     port: 5173,
